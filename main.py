fde9f911d5d8058fe5e0ea32f8f7a54bdb2b5656
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-06 08:40:33 +0800

    set initial fps logic
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,7 @@ def main():
                 return
         screen.fill("#000000")
         pygame.display.update()
+        clock.tick(60)
         
 if __name__ == "__main__":
     main()
