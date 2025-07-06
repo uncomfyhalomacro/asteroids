c870965ce72277b86ccd0ef8e280df7e0415fd91
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-06 20:10:48 +0800

    apply performance tips from https://www.codeproject.com/Articles/5298051/Improving-Performance-in-Pygame-Speed-Up-Your-Game
    Sadly, I mistakenly update the whole screen every time an entity is updated. i should update them all first before re-rendering
    the whole screen. this has caused a performance regression even for a tiny game lmfao
    
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from player import Player, Bullet
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 import pygame
+from pygame.locals import *
 import sys
 import random
 
@@ -13,9 +14,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    pygame.event.set_allowed([QUIT, KEYDOWN, KEYUP])
     clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    flags = DOUBLEBUF
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), flags, 16)
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -27,26 +30,27 @@ def main():
     asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     display = pygame.display
+    asteroids_destroyed = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill("#000000")
-        updatable.update(dt)
         for asteroid in asteroids:
             for bullet in bullets:
                 if bullet.has_collided_with(asteroid):
-                    print("bullet has hit an asteroid!")
                     bullet.kill()
-                    asteroid.split(screen)
+                    asteroids_destroyed += 1 if asteroid.split(screen) else 0
 
             if player.has_collided_with(asteroid):
                 print("Game over!")
+                print(f"Number of asteroids destroyed before succumbing to skill issue: {asteroids_destroyed}")
                 sys.exit(1)
         for entity in drawable:
             entity.draw(screen)
-            display.update()
+        display.update()
         dt = clock.tick(60) / 1000
+        screen.fill("#000000")
+        updatable.update(dt)
 
 
 if __name__ == "__main__":
